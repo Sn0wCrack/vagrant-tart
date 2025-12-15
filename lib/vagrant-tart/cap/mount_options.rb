bbb040932f77e72150a6410617f7a69ac858174a
Sn0wCrack <442287+Sn0wCrack@users.noreply.github.com>
2025-12-15 15:46:51 +1100

    fix: mount options for systemd based distros
    fix: tag mount option causing issues mounting directory

--- lib/vagrant-tart/cap/mount_options.rb
+++ lib/vagrant-tart/cap/mount_options.rb
@@ -10,6 +10,7 @@ module VagrantPlugins
     module Cap
       # Capability for mount options
       module MountOptions
+        extend Vagrant::Util::GuestInspection::Linux
         extend VagrantPlugins::SyncedFolder::UnixMountHelpers
 
         # Mount type for VirtFS
@@ -22,13 +23,26 @@ module VagrantPlugins
         # @param [String] path of mount on guest
         # @param [Hash] hash of mount options
         def self.mount_options(machine, _name, guest_path, options)
+          comm = machine.communicate
           mount_options = options.fetch(:mount_options, [])
           detected_ids = detect_owner_group_ids(machine, guest_path, mount_options, options)
           mount_uid = detected_ids[:uid]
           mount_gid = detected_ids[:gid]
 
+          # Remove non-filesystem related mount options
+          mount_options = mount_options.reject { |opt| opt.include?("tag=") || opt.include?("mode=") }
+
           # virtiofs mount options
-          mount_options << "_netdev"
+
+          # For operating systems using systemd, we need to specify automounting and running prior-to remove filesystems being mounted
+          # This mimics the behaviour of _netdev in standard systems
+          if systemd?(comm)
+            mount_options << "x-systemd.automount"
+            mount_options << "x-systemd.before=remote-fs.target"
+          else
+            mount_options << "_netdev"
+          end
+
           mount_options << "nofail"
 
           mount_options = mount_options.join(",")
